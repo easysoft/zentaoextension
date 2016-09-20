41a48eeab7d5a1933417b372268451ffabb6d456
liugang <liugang@cnezsoft.com>
2016-09-20 16:37:39 +0800

    * Add scripts to merge and separate lang files.

---
+++ language/mergelang.php
@@ -0,0 +1,50 @@
+#!/usr/bin/env php
+<?php
+/**
+ * The script can merge zh-cn language files of zentaoPMS to one file.  
+ */
+if(empty($argv[1])) die("Must give a zentaopms root path.\n");
+
+$zentaoRoot = $argv[1];                        /* Root of zentaoPMS. */
+$mergedPath = empty($argv[2]) ? '' : $argv[2]; /* The merged file will be write in this directory. */
+$moduleRoot = $zentaoRoot . DIRECTORY_SEPARATOR . 'module' . DIRECTORY_SEPARATOR;
+$handle     = opendir($moduleRoot);  
+if($handle)  
+{  
+    $langFiles = array();
+    while(($module = readdir($handle)) !== false)  
+    {  
+        $filePath = $moduleRoot . $module . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . 'zh-cn.php';
+        if(!file_exists($filePath)) continue;
+
+        /* Remove the comments and start tag of php. */
+        $langFiles[$module] = preg_replace("/\/\*.*?\*\/\n/s", '', ltrim(file_get_contents($filePath), "<?php"));
+    }  
+    closedir($handle);
+
+    $mergedFile = 'lang.zh-cn.php';
+    if($mergedPath)
+    {
+        if(!file_exists($mergedPath)) @mkdir($mergedPath, 0777, true);
+        $mergedFile = $mergedPath . DIRECTORY_SEPARATOR . $mergedFile;
+    }
+    file_put_contents($mergedFile, "<?php\n");
+    $fh = @fopen($mergedFile, 'a');
+    ksort($langFiles);
+
+    if(isset($langFiles['common']))
+    {
+        fwrite($fh, "/* common */");
+        fwrite($fh, $langFiles['common']);
+        unset($langFiles['common']);
+    }
+
+    foreach($langFiles as $module => $content)
+    {
+        fwrite($fh, "/* " . $module . " */");
+        fwrite($fh, $content);
+    }
+    fclose($fh);
+}
+
+echo "merge lang files down!\n";
---
+++ language/separatelang.php
@@ -0,0 +1,36 @@
+#!/usr/bin/env php
+<?php
+/**
+ * The script can separate the merged language file into each module. 
+ */
+if(empty($argv[1])) die("Must give a language file path.\n");
+
+$filePath = $argv[1];                        /* The merged language file, such as lang.en.php. */
+$extPath  = empty($argv[2]) ? '' : $argv[2]; /* The separated files will be write in this directory. */
+$filename = explode('.', basename($filePath));
+if(count($filename) != 3 || $filename[0] != 'lang' || $filename[2] != 'php') die("The format of language filename must be lang.xxx.php. xxx is the abbreviation of language, such as lang.en.php.\n");
+
+$lang     = $filename[1];
+$content  = ltrim(file_get_contents($filePath), "<?php\n");;
+preg_match_all("/\/\*.*?\*\//s", $content, $out);
+$modules  = $out[0];
+if(!$modules) die("$filePath can't be separated because the file doesn't have the module marks such as '/* common */'.\n");
+
+define('MODULE_SEPARATOR', '/******module_separator******/');
+
+$content    = str_replace($modules, MODULE_SEPARATOR, $content);
+$contents   = explode(MODULE_SEPARATOR, ltrim($content, MODULE_SEPARATOR));
+
+$moduleRoot = "extLang_$lang" . DIRECTORY_SEPARATOR . 'module' . DIRECTORY_SEPARATOR;
+if($extPath) $moduleRoot = $extPath . DIRECTORY_SEPARATOR . $moduleRoot;
+@mkdir($moduleRoot, 0777, true);
+@chdir($moduleRoot);
+foreach($modules as $key => $module)
+{
+    $module  = trim(rtrim(ltrim($module, '/*'), '*/'));
+    $langDir = $module . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR;
+    @mkdir($langDir, 0777, true);
+    if(is_dir($langDir)) file_put_contents($langDir . $lang . '.php', "<?php" . $contents[$key]);
+}
+
+echo "separate lang file down!\n";
