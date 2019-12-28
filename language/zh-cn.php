582b03e9e07330732a43ab670eb1ae359f7c6740
chunsheng <chunsheng@cnezsoft.com>
2019-12-29 03:20:31 +0800

    Update lang files

--- language/zh-cn.php
+++ language/zh-cn.php
@@ -4352,7 +4352,7 @@ $lang->project->treeOnlyTask      = '树状图只看任务';
 $lang->project->treeOnlyStory     = "树状图只看{$lang->storyCommon}";
 $lang->project->storyKanban       = "{$lang->storyCommon}看板";
 $lang->project->storySort         = "{$lang->storyCommon}排序";
-$lang->project->importPlanStory   = "创建" . $lang->projectCommon . "成功！是否导入计划关联的相关{$lang->storyCommon}？";
+$lang->project->importPlanStory   = '创建' . $lang->projectCommon . '成功！\n是否导入计划关联的相关' . $lang->storyCommon . '？';
 $lang->project->iteration         = '版本迭代';
 $lang->project->iterationInfo     = '迭代%s次';
 $lang->project->viewAll           = '查看所有';
@@ -4848,14 +4848,14 @@ $lang->report->annualData->logins           = "累计登录次数";
 $lang->report->annualData->actions          = "累计动态数";
 $lang->report->annualData->efforts          = "累计日志数";
 $lang->report->annualData->consumed         = "累计工时数";
-$lang->report->annualData->foundBugs        = "累计发现Bug数";
+$lang->report->annualData->foundBugs        = "累计创建Bug数";
 $lang->report->annualData->createdCases     = "累计创建用例数";
 $lang->report->annualData->involvedProducts = "累计参与{$lang->productCommon}数";
 $lang->report->annualData->createdPlans     = "累计创建计划数";
 $lang->report->annualData->createdStories   = "累计创建{$lang->storyCommon}数";
 
-$lang->report->annualData->productOverview = "{$lang->productCommon}创建的{$lang->storyCommon}数及占比";
-$lang->report->annualData->qaOverview      = "{$lang->productCommon}创建的Bug数及占比";
+$lang->report->annualData->productOverview = "{$lang->productCommon}创建{$lang->storyCommon}数及占比";
+$lang->report->annualData->qaOverview      = "{$lang->productCommon}创建Bug数及占比";
 $lang->report->annualData->projectOverview = "参与{$lang->projectCommon}概览";
 $lang->report->annualData->doneProject     = "已完成的{$lang->projectCommon}";
 $lang->report->annualData->doingProject    = "正在进行的{$lang->projectCommon}";
@@ -4864,27 +4864,28 @@ $lang->report->annualData->suspendProject  = "已挂起的{$lang->projectCommon}
 $lang->report->annualData->projectName   = "{$lang->projectCommon}名称";
 $lang->report->annualData->finishedStory = "完成{$lang->storyCommon}数";
 $lang->report->annualData->finishedTask  = '完成任务数';
-$lang->report->annualData->foundBug      = '发现Bug数';
+$lang->report->annualData->foundBug      = '创建Bug数';
 $lang->report->annualData->resolvedBug   = '解决Bug数';
 $lang->report->annualData->productName   = "{$lang->productCommon}名称";
 $lang->report->annualData->planCount     = '计划数';
 $lang->report->annualData->storyCount    = "{$lang->storyCommon}数";
 
-$lang->report->annualData->qaData           = "累计发现的Bug数和创建的用例数";
-$lang->report->annualData->totalCreatedBug  = '累计发现的Bug数';
-$lang->report->annualData->totalCreatedCase = '累计创建的用例数';
+$lang->report->annualData->qaData           = "累计创建Bug数和创建用例数";
+$lang->report->annualData->totalCreatedBug  = '累计创建Bug数';
+$lang->report->annualData->totalCreatedCase = '累计创建用例数';
 
-$lang->report->annualData->devData           = "完成的任务数和解决的Bug数";
-$lang->report->annualData->totalFinishedTask = '完成的任务数';
-$lang->report->annualData->totalResolvedBug  = '解决的Bug数';
+$lang->report->annualData->devData           = "完成任务数和解决Bug数";
+$lang->report->annualData->totalFinishedTask = '完成任务数';
+$lang->report->annualData->totalResolvedBug  = '解决Bug数';
+$lang->report->annualData->totalConsumed     = '累计工时';
 
-$lang->report->annualData->poData          = "所创建的{$lang->storyCommon}数对应的优先级及状态";
+$lang->report->annualData->poData          = "所创建{$lang->storyCommon}数和对应的优先级及状态";
 $lang->report->annualData->totalStoryPri   = "创建{$lang->storyCommon}优先级分布";
 $lang->report->annualData->totalStoryStage = "创建{$lang->storyCommon}阶段分布";
 
-$lang->report->annualData->qaStatistics  = "月发现的Bug数和创建的用例数";
+$lang->report->annualData->qaStatistics  = "月创建Bug数和创建用例数";
 $lang->report->annualData->poStatistics  = "月创建{$lang->storyCommon}数";
-$lang->report->annualData->devStatistics = "月完成任务累计工时";
+$lang->report->annualData->devStatistics = "月完成任务数及累计工时和解决Bug数";
 
 $lang->report->annualData->unit = "个";
 /* score */
