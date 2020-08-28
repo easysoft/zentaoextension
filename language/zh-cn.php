86b9d725444219b6ee9f5bd9482e00d3d24d81bc
chunsheng <chunsheng@cnezsoft.com>
2020-08-29 03:20:42 +0800

    Update lang files

--- language/zh-cn.php
+++ language/zh-cn.php
@@ -2931,44 +2931,44 @@ $lang->dept->error = new stdclass();
 $lang->dept->error->hasSons  = '该部门有子部门，不能删除！';
 $lang->dept->error->hasUsers = '该部门有职员，不能删除！';
 /* design */
-$lang->design->common        = '设计';
-$lang->design->browse        = '浏览列表';
-$lang->design->commit        = '关联提交';
+$lang->design->id            = '编号';
+$lang->design->name          = '设计名称';
+$lang->design->story         = '需求';
+$lang->design->type          = '设计类型';
+$lang->design->ditto         = '同上';
 $lang->design->submission    = '相关提交';
 $lang->design->version       = '版本号';
 $lang->design->assignedTo    = '指派给';
 $lang->design->actions       = '操作';
-$lang->design->delete        = '删除';
-$lang->design->confirmDelete = '您确定要删除这个设计吗？';
-$lang->design->edit          = '变更';
 $lang->design->byQuery       = '搜索';
 $lang->design->products      = '所属产品';
 $lang->design->story         = '相关需求';
 $lang->design->file          = '附件';
-$lang->design->id            = '编号';
-$lang->design->story         = '需求';
-$lang->design->ditto         = '同上';
-$lang->design->type          = '设计类型';
-$lang->design->name          = '设计名称';
-$lang->design->create        = '创建设计';
-$lang->design->batchCreate   = '批量创建';
-$lang->design->view          = '设计概况';
 $lang->design->desc          = '设计描述';
+$lang->design->range         = '影响范围';
 $lang->design->product       = '所属产品';
 $lang->design->basicInfo     = '基础信息';
 $lang->design->commitBy      = '由谁提交';
 $lang->design->commitDate    = '提交时间';
 $lang->design->affectedStory = "影响{$lang->storyCommon}";
 $lang->design->affectedTasks = '影响任务';
-$lang->design->submit        = '提交评审';
-$lang->design->revision      = '查看关联代码';
-$lang->design->designView    = '查看详情';
 $lang->design->reviewObject  = '评审对象';
 $lang->design->createdBy     = '由谁创建';
 $lang->design->createdDate   = '创建时间';
 $lang->design->basicInfo     = '基本信息';
-$lang->design->assignTo      = '指派';
 $lang->design->noAssigned    = '未指派';
+
+$lang->design->common      = '设计';
+$lang->design->create      = '创建设计';
+$lang->design->batchCreate = '批量创建';
+$lang->design->edit        = '变更';
+$lang->design->delete      = '删除';
+$lang->design->view        = '设计概况';
+$lang->design->browse      = '浏览列表';
+$lang->design->commit      = '关联提交';
+$lang->design->submit      = '提交评审';
+$lang->design->assignTo    = '指派';
+$lang->design->revision    = '查看关联代码';
 
 $lang->design->typeList         = array();
 $lang->design->typeList['']     = '';
@@ -2977,17 +2977,17 @@ $lang->design->typeList['DDS']  = '详细设计';
 $lang->design->typeList['DBDS'] = '数据库设计';
 $lang->design->typeList['ADS']  = '接口设计';
 
-$lang->design->range          = '影响范围';
-$lang->design->errorSelection = '还没有选中记录!';
-$lang->design->noDesign       = '暂时没有记录';
-$lang->design->noCommit       = '暂时没有提交记录';
-
 $lang->design->rangeList           = array();
 $lang->design->rangeList['all']    = '全部记录';
 $lang->design->rangeList['assign'] = '选中记录';
 
 $lang->design->featureBar['all'] = '所有';
 $lang->design->featureBar += $lang->design->typeList;
+
+$lang->design->errorSelection = '还没有选中记录!';
+$lang->design->noDesign       = '暂时没有记录';
+$lang->design->noCommit       = '暂时没有提交记录';
+$lang->design->confirmDelete  = '您确定要删除这个设计吗？';
 /* dev */
 $lang->dev->common     = '二次开发';
 $lang->dev->api        = 'API';
@@ -5501,7 +5501,7 @@ $lang->repo->error->noPriv        = '程序没有权限切换到目录 %s';
 $lang->repo->error->output        = "执行命令：%s\n错误结果(%s)： %s\n";
 $lang->repo->error->clientVersion = "客户端版本过低，请升级或更换SVN客户端";
 $lang->repo->error->encoding      = "编码可能错误，请更换编码重试。";
-$lang->repo->error->deleted       = "删除版本库失败，当前版本库已与设计有相关提交";
+$lang->repo->error->deleted       = "删除版本库失败，当前版本库已与设计关联提交";
 
 $lang->repo->syncTips      = '请参照<a target="_blank" href="https://www.zentao.net/book/zentaopmshelp/207.html">这里</a>，设置版本库定时同步。';
 $lang->repo->encodingsTips = "提交日志的编码，可以用逗号连接起来的多个，比如utf-8。";
